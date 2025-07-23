7eceb764ba5ffb958854a35e5568cc34fab337d5
e4avwork <e4avwork@gmail.com>
2025-07-23 14:00:58 +0200

    refactor: remove App component

--- src/App.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-// Simple smoke test
-
-describe("App", () => {
-  it("should run a basic test", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
---
+++ src/components/Spinner/Spinner.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+import Spinner from "./Spinner";
+
+describe("Spinner", () => {
+  it("should render", () => {
+    expect(Spinner).toBeDefined();
+  });
+});
